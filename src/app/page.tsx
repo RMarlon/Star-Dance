25cbe4fd375fc18abf526dbcd29de43cbe67ae99
Marlon <xsantos.marlonx@gmail.com>
2025-08-29 12:24:58 -0300

    concluido a responsividade e funcionamento do menu mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
+"use client"
+
 import { MobileMenu } from "@/components/MobileMenu";
 import { NavBar } from "@/components/NavBar";
 import { listMobileMenu } from "@/data/listMobileMenu";
 import { listNav } from "@/data/listNav";
 
 const Page = () =>{
+
+  const toggleMenu = () =>{
+      const menu = document.querySelector('#menu');
+      menu?.classList.toggle('translate-x-full');
+  }
+
   return(
     <div className="container mx-auto bg-white h-screen rounded-md shadow-2xl shadow-gray-600">
 
       <section>
-        <MobileMenu mobileNav={listMobileMenu}/>
+        <MobileMenu mobileNav={listMobileMenu} openMenu={toggleMenu} closeMenu={toggleMenu}/>
       </section>
 
       <section>
--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -3,26 +3,31 @@ import Link from "next/link";
 
 type PropsMobile = {
     mobileNav: MobileNavBar[];
+    openMenu: () => void;
+    closeMenu:() => void;
 }
 
-export const MobileMenu = ({ mobileNav }: PropsMobile) => {
+export const MobileMenu = ({ mobileNav, openMenu, closeMenu }: PropsMobile) => {
+    
     return (
         <>
             {mobileNav.map(menu => (
-                <div key={menu.id} className="font-display text-black">
+                <div key={menu.id} className="font-display">
 
-                    <div className="relative flex justify-between items-center">
-                        <img src={menu.images} alt="imagem da logo" className="w-40 h-40 rounded-full"/>
-                        <button className="absolute right-10 top-8">
-                            <img src={menu.imagesMenuBurger} alt="botão menu hamburger" className="w-10" />
+                    <div className="relative flex justify-between items-center sm:hidden">
+                        <img src={menu.images} alt="imagem da logo" className="w-40 h-40 rounded-full" />
+
+                        <button onClick={openMenu} className="absolute right-10 top-10 cursor-pointer">
+                            <img id="imgBurger" src={menu.imagesMenuBurger} alt="botão menu hamburger" className="w-10" />
                         </button>
                     </div>
 
-                    <div className="w-70 h-screen fixed top-0 right-0 border translate-x-full  duration-300 ease-in-out">
+                    <div id="menu" className="w-70 h-screen fixed top-0 right-0  translate-x-full duration-300 ease-in-out bg-black">
 
                         <div className="flex justify-between items-center px-10 pt-4">
                             <h1 className="">Star Dance ⭐</h1>
-                            <button className="cursor-pointer">
+
+                            <button onClick={closeMenu} className="cursor-pointer">
                                 <img src={menu.imagesClose} alt="botão para fechar o menu" />
                             </button>
                         </div>
@@ -32,7 +37,7 @@ export const MobileMenu = ({ mobileNav }: PropsMobile) => {
                             <li>
                                 <Link href={menu.about[1]}
                                     className="hover:border-b hover:border-purple-700 hover:pb-2
-                                    duration-300 ease-in-out font-bold 
+                                    duration-300 ease-in-out 
                                 ">
 
                                     {menu.about[0]}
@@ -41,7 +46,7 @@ export const MobileMenu = ({ mobileNav }: PropsMobile) => {
                             <li>
                                 <Link href={menu.galery[1]}
                                     className="hover:border-b hover:border-purple-700 hover:pb-2
-                                    duration-300 ease-in-out font-bold
+                                    duration-300 ease-in-out 
                                 ">
                                     {menu.galery[0]}
                                 </Link>
@@ -49,7 +54,7 @@ export const MobileMenu = ({ mobileNav }: PropsMobile) => {
                             <li>
                                 <Link href={menu.videos[1]}
                                     className="hover:border-b hover:border-purple-700 hover:pb-2
-                                    duration-300 ease-in-out font-bold
+                                    duration-300 ease-in-out 
                                 ">
                                     {menu.videos[0]}
                                 </Link>
@@ -57,7 +62,7 @@ export const MobileMenu = ({ mobileNav }: PropsMobile) => {
                             <li>
                                 <Link href={menu.audition[1]}
                                     className="hover:border-b hover:border-purple-700 hover:pb-2
-                                    duration-300 ease-in-out font-bold
+                                    duration-300 ease-in-out 
                                 ">
                                     {menu.audition[0]}
                                 </Link>
