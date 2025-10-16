913c6e82c7a8a30d00150f3bf17b58fd3dbd94a5
Marlon <xsantos.marlonx@gmail.com>
2025-10-16 11:21:36 -0300

    corrigindo o problema de troca de branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import { Banner } from "@/components/Banner";
 import { Galery } from "@/components/Galery";
 import { MobileMenu } from "@/components/MobileMenu";
 import { NavBar } from "@/components/NavBar";
+import { Videos } from "@/components/Videos";
 import { listAbout } from "@/data/listAbout";
 import { listBanner } from "@/data/listBanner";
 import { listGalery } from "@/data/listGalery";
 import { listMobileMenu } from "@/data/listMobileMenu";
 import { listNav } from "@/data/listNav";
+import { listVideos } from "@/data/listVideos";
 
 
 const Page = () => {
@@ -29,7 +31,14 @@ const Page = () => {
       <NavBar menu={listNav} />
       <Banner banner={listBanner} />
       <About we={listAbout} />
-      <Galery img={listGalery} handleClick={() => { }} />
+      
+      <section className="pb-10">
+        <Galery img={listGalery} handleClick={() => { }} />
+      </section>
+      
+      <section className="pb-10">
+        <Videos play={listVideos}/>
+      </section>
 
     </div>
   );
---
+++ src/components/Videos.tsx
@@ -0,0 +1,41 @@
+import { VideosUs } from "@/types/VideosUs";
+
+type PropsVideos = {
+    play: VideosUs[];
+}
+
+export const Videos = ({play}:PropsVideos) => {
+    return (
+        <div className="bg-black">
+            <h2 className="flex items-center justify-between font-display text-2xl text-white text-right py-6 px-10">
+                Vídeos Star Dance
+                <img src="images/videos.png" alt="imagem icone videos" className="w-15"/>
+            </h2>
+            <div className="border-b text-gray-500"></div>
+            <div className="flex justify-around items-center">
+
+                <div className="hidden md:block ml-10">
+                    <img src="images/logo2.png" alt="imagem logo" className="w-60 h-30"/>
+                </div>
+
+                <p className="text-gray-600 text-lg px-10 py-10 font-display-1 lg:py-0">
+                    Essas são nossas imagens, elas mostram nossos momentos em apresentações e ensaios.
+                    Cada um deles é o que mantem nosso grupo unido e atuando até hoje. Somos gratos a esses momentos, e
+                    sei que outros estão por vir, e sempre daremos nosso melhor!
+                </p>
+            </div>
+
+            <div
+                className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8
+                bg-gray-800 bg-cover shadow-2xl shadow-white py-2 px-2
+                bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% xl:rounded-md"
+            >
+                {play.map(item =>(
+                    <>
+                    
+                    </>
+                ))}
+            </div>
+        </div>
+    );
+}
