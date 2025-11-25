eba8ffd7e0bcc73a0954d730a263e00fd3a76b5d
Marlon <xsantos.marlonx@gmail.com>
2025-11-25 10:25:15 -0300

    alterando a estética da galeria

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -2,6 +2,7 @@ import { listGalery } from "@/data/listGalery";
 import { GaleryUs } from "@/types/GaleryUs";
 import { useState } from "react";
 import { Modal } from "./Modal";
+import '../app/galery.css';
 
 type PropsGaley = {
     img: GaleryUs[];
@@ -26,38 +27,70 @@ export const Galery = ({ img, handleClick }: PropsGaley) => {
 
     return (
         <div className="bg-white">
-            <h2 className="flex items-center justify-between font-display text-2xl text-black text-right py-6 px-10">
+            <h2 className="flex items-center justify-between font-display text-2xl text-black text-right py-6 px-10 ">
                 <img src="images/moudura.png" alt="imagem de moudura" />
                 Galeria Star Dance
             </h2>
             <div className="border-b text-gray-500"></div>
-            <div className="flex justify-around items-center">
-                <p className="text-gray-600 text-lg px-10 py-10 font-display-1 lg:py-0">
-                    Essas são nossas imagens, elas mostram nossos momentos em apresentações e ensaios.
-                    Cada um deles é o que mantem nosso grupo unido e atuando até hoje. Somos gratos a esses momentos, e
-                    sei que outros estão por vir, e sempre daremos nosso melhor!
-                </p>
-                <div className="hidden md:block mr-10">
-                    <img src="images/logo2.png" alt="imagem logo" className="w-60 h-40"/>
+            
+            <div id="body" className="body">
+                <div id="wrapper" className="wrapper">
+                    <div className="cover cover-left"></div>
+                    <div className="cover cover-right turn"></div>
+
+                    <div className="book font-display-1">
+                        <div className="book-page page-left">
+                            <div className="profile-page">
+                                <img src="images/logo2.png" alt="logo Star Dance" className="logo" />
+                                <h1 className="title font-display">Star Dance</h1>
+                                <h3>Cover K-pop</h3>
+
+                                <div className="social-media">
+
+                                </div>
+
+                                <p>
+                                    Essas são nossas imagens, elas mostram nossos momentos em apresentações e ensaios.
+                                    Cada um deles é o que mantem nosso grupo unido e atuando até hoje. Somos gratos a esses momentos, e
+                                    sei que outros estão por vir, e sempre daremos nosso melhor!
+                                </p>
+
+                                <div className="btn-box">
+                                    <a href="" className="btn contact-me" >Venha fazer parte</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* page 1 & 2 */}
+                        <div className="book-page page-right" id="turn-1">
+                            <div className="page-front">
+                                <h1 className="title">Memorias</h1>
+
+                                <div className="workeduc-box">
+                                    <div className="workeduc-content">
+                                        <span className="year">2020 - 2021</span>
+                                        <h3>Nosso desenvolvimento</h3>
+                                        <div>
+                                            {img.map(item => (
+                                                <>
+                                                    <div key={item.id} onClick={() => openModal(item.id)} className="cursor-pointer">
+                                                        <img  src={item.image} alt="imagens do grupo star dance" className="w-[450px] h-[250px] pt-2 rounded-md duration-500 transition ease-in-out hover:opacity-80" />
+                                                    </div>
+                                                </>
+                                            ))}
+                                        </div>
+                                        {modal &&
+                                            <Modal image={imageModal} closeModal={closeModal} />
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="page-back">
+                                <h1 className="title">Eventos</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div
-                className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8
-                bg-gray-800 bg-cover shadow-2xl shadow-black py-2 px-2
-                bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% xl:rounded-md">
-
-                {img.map(item => (
-                    <>
-                        <div onClick={() => openModal(item.id)} key={item.id} className="cursor-pointer">
-                            <img src={item.image} alt="imagens do grupo star dance" className="w-full rounded-md duration-500 transition ease-in-out hover:opacity-80" />
-                        </div>
-                    </>
-                ))}
-            </div>
-            {modal &&
-                <Modal image={imageModal} closeModal={closeModal} />
-            }
         </div>
     );
 }
--- src/data/listGalery.ts
+++ src/data/listGalery.ts
@@ -1,12 +1,13 @@
 import { GaleryUs } from "@/types/GaleryUs";
 
 export const listGalery: GaleryUs[] = [
-    // { id: 1, image: 'images/img1.jpg' },
-    { id: 2, image: 'images/img2.jpg' },
-    { id: 3, image: 'images/img3.jpg' },
-    { id: 4, image: 'images/img4.jpg' },
-    { id: 5, image: 'images/img5.jpg' },
-    { id: 6, image: 'images/img6.jpg' },
-    { id: 7, image: 'images/img7.jpg' },
-    // { id: 8, image: 'images/img8.jpg' },
+    
+    { 
+        id: 0, 
+        image: 'images/img2.jpg',
+    },{ 
+        id: 1, 
+        image: 'images/img3.jpg',
+    },
+ 
 ];
